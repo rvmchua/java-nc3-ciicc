d4c1b657435ba36792c84155181f0de6d0fa9ed4
r-chua <rvmchua@gmail.com>
2026-07-18 15:50:02 +0800

    Add NumberPrinter Act 13

---
+++ src/NumberPrinter1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class NumberPrinter1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int totalNumber = input.nextInt();
+
+        input.close();
+        for (int numberToDisplay = 1; numberToDisplay <= totalNumber; numberToDisplay++) {
+            System.out.println(numberToDisplay);
+        }
+    }
+}
